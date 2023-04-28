322d852e01088597f0c6d223fb3076639ca3fb2c
Volodymyr Dovgopol <37467370+vidovgopol@users.noreply.github.com>
2023-04-28 11:17:14 +0000

    commit check

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -39,7 +39,7 @@ to quickly create a Cobra application.`,
 			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
 		})
 		if err != nil {
-			log.Fatal("Please check TELE_TOKEN env variable. %s", err)
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
 			return
 		}
 
